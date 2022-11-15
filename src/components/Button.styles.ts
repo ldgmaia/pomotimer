ba3fe61e2003fb1036a2a7def42fbc31d218d30d
Diego Maia <ldgmaia@gmail.com>
2022-11-15 13:47:38 -0500

    ESLint configured

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,6 +1,6 @@
-import styled, { css } from 'styled-components';
+import styled, { css } from 'styled-components'
 
-export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success';
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success'
 
 interface ButtonContainerProps {
   variant: ButtonVariant
@@ -14,20 +14,18 @@ const buttonVariants = {
 }
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
-
   width: 100px;
   height: 40px;
   border-radius: 6px;
   margin: 5px;
   border: 0;
 
-  background-color: ${props => props.theme['green-500']};
-  color: ${props => props.theme.white};
+  background-color: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme.white};
 
-  /* ${props => {
+  /* ${(props) => {
     return css`
-      background-color: ${buttonVariants[props.variant]}
+      background-color: ${buttonVariants[props.variant]};
     `
   }} */
-
 `
